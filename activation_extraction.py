be523e1d0c16a3de65dcc203c725ffe9386d1c45
Laurent Perrinet <laurent.perrinet@univ-amu.fr>
2020-10-13 09:47:24 +0200

    Update activation_extraction.py
    PEP 8

--- activation_extraction.py
+++ activation_extraction.py
@@ -17,7 +17,7 @@ root = '/nfs/a1/userhome/liuxingyu/workingdir/coding_sparseness'
 net = 'AlexNet' # 'AlexNet', 'Vgg11'
 null_method = 'permut_weight_kernel'  # 'permut_weight', 'permut_weight_chn', 'permut_weight_kernel', 'permut_bias', 'norelu', 'None'
 relu = True
-dataset = 'imagenet' 
+dataset = 'imagenet'
 n = 10
 
 if dataset == 'imagenet':
@@ -32,12 +32,12 @@ if relu is True:
 elif relu is False:
     pf = ''
     sublayer = 'conv'
-    
+
 if net == 'AlexNet':
     layer_name = ['conv1' + pf, 'conv2' + pf, 'conv3' + pf, 'conv4' + pf,
                   'conv5' + pf, 'fc1' + pf, 'fc2' + pf]
 elif net == 'Vgg11':
-    layer_name = ['conv1' + pf, 'conv2' + pf, 'conv3' + pf, 'conv4' + pf, 
+    layer_name = ['conv1' + pf, 'conv2' + pf, 'conv3' + pf, 'conv4' + pf,
                   'conv5' + pf, 'conv6' + pf, 'conv7' + pf, 'conv8' + pf,
                   'fc1' + pf, 'fc2' + pf]
 
@@ -49,7 +49,7 @@ stimuli = Stimulus()
 stimuli.load(stim_path) # load stimuli
 dmask = gen_dmask(layer_name)  # generate DNN mask
 
-# permut weights pretrained network for each layer, finest granularity purmutation    
+# permut weights pretrained network for each layer, finest granularity purmutation
 def permut_weight_model(dnn):
     new_dict = {}
     for key in dnn.model.state_dict().keys():
@@ -60,7 +60,7 @@ def permut_weight_model(dnn):
                     torch.randperm(*para.view(-1).size())].reshape(ori_shape)
         else:
             new_dict[key] = dnn.model.state_dict()[key]
-        
+
     dnn.model.load_state_dict(new_dict)
 
 # permut weights and bias of pretrained network for each layer, kernel-wise permuation
@@ -70,11 +70,11 @@ def permut_weight_kernel_model(dnn):
         if key.split('.')[-1] == 'weight':
             para = dnn.model.state_dict()[key]
             ori_shape = para.shape
-            new_dict[key] = para.view([para.shape[0], -1])[ 
+            new_dict[key] = para.view([para.shape[0], -1])[
                     :, torch.randperm(para.view([para.shape[0], -1]).size()[-1])].reshape(ori_shape)
         else:
             new_dict[key] = dnn.model.state_dict()[key]
-            
+
     dnn.model.load_state_dict(new_dict)
 
 # permut weights pretrained network for each layer, chn-wise permuation
@@ -86,10 +86,10 @@ def permut_weight_chn_model(dnn):
             new_dict[key] = para[torch.randperm(para.shape[0])]
         else:
             new_dict[key] = dnn.model.state_dict()[key]
-        
+
     dnn.model.load_state_dict(new_dict)
 
-# permut bias pretrained network for each layer, 
+# permut bias pretrained network for each layer,
 def permut_bias_model(dnn):
     new_dict = {}
     for key in dnn.model.state_dict().keys():
@@ -100,7 +100,7 @@ def permut_bias_model(dnn):
                     torch.randperm(*para.view(-1).size())].reshape(ori_shape)
         else:
             new_dict[key] = dnn.model.state_dict()[key]
-        
+
     dnn.model.load_state_dict(new_dict)
 
 # deactivate ReLU for each layer
@@ -108,7 +108,7 @@ def norelu_model(dnn):
     class Deact_ReLU(nn.Module):
         def __init__(self):
             super(Deact_ReLU, self).__init__()
-        
+
         def forward(slef, x):
             return x
 
@@ -118,7 +118,7 @@ def norelu_model(dnn):
                 setattr(model, child_name, Deact_ReLU())
             else:
                 replace_relu_to_none(child)
-    
+
     replace_relu_to_none(dnn.model)
 
 
@@ -128,24 +128,24 @@ if null_method is None or null_method == 'norelu':
 
     if null_method == 'norelu':
         # prepare model
-        norelu_model(dnn)    
+        norelu_model(dnn)
         # save path
-        out_dir = os.path.join(root, '{0}_{1}'.format(net.lower(), null_method), 
+        out_dir = os.path.join(root, '{0}_{1}'.format(net.lower(), null_method),
                                 'dnn_activation')
         if os.path.exists(out_dir) is not True:
-            os.makedirs(out_dir)          
+            os.makedirs(out_dir)
         out_path = os.path.join(out_dir, '{0}_{1}_{2}_mean_{3}.act.h5'.format(
                 net.lower(), null_method, sublayer, dataset))#
-    
+
     elif null_method is None:
         # save path
         out_dir = os.path.join(root, net.lower(),'dnn_activation')
         if os.path.exists(out_dir) is not True:
             os.makedirs(out_dir)
-            
+
         out_path = os.path.join(out_dir, '{0}_{1}_mean_{2}.act.h5'.format(
                 net.lower(), sublayer, dataset))
-    
+
     # extract activation
     activation = dnn.compute_activation(stimuli, dmask, 'mean', cuda=True)
     # save act
@@ -155,35 +155,35 @@ if null_method is None or null_method == 'norelu':
 #%%
 # extract activation from weight permuted model
 if null_method in ['permut_weight', 'permut_weight_chn', 'permut_weight_kernel', 'permut_bias']:
-    for i in range(n):   
-        
+    for i in range(n):
+
         # prepare model
-        if null_method == 'permut_weight':       
+        if null_method == 'permut_weight':
             permut_weight_model(dnn)
-        elif null_method == 'permut_weight_chn':       
+        elif null_method == 'permut_weight_chn':
             permut_weight_chn_model(dnn)
-        elif null_method == 'permut_weight_kernel':       
+        elif null_method == 'permut_weight_kernel':
             permut_weight_kernel_model(dnn)
-        elif null_method == 'permut_bias':       
+        elif null_method == 'permut_bias':
             permut_bias_model(dnn)
-            
+
         # extract activation-
         activation = dnn.compute_activation(stimuli, dmask, 'mean', cuda=True)
-        
+
         # compute catwise mean and replace the original stimuliwise act
         for layer in list(activation.layers):
             cur_shape = np.shape(activation.get(layer))
-            new_shape = np.r_[int(cur_shape[0] / stim_per_cat), 
+            new_shape = np.r_[int(cur_shape[0] / stim_per_cat),
                               stim_per_cat, cur_shape[1:]]
             cat_data = activation.get(layer).reshape(new_shape)
             activation.set(layer, cat_data.mean(1))
-         
+
         # save act
-        out_dir = os.path.join(root, '{0}_{1}'.format(net.lower(), null_method), 
+        out_dir = os.path.join(root, '{0}_{1}'.format(net.lower(), null_method),
                                 'dnn_activation')
         if os.path.exists(out_dir) is not True:
             os.makedirs(out_dir)
-            
+
         out_path = os.path.join(out_dir, '{0}_{1}_{2}_mean_{3}_{4}.act.h5'.format(
                 net.lower(), null_method, sublayer, dataset, i))
         activation.save(out_path)
